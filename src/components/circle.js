668f63057d0922fd8372e953d7c82387c7dd7d63
Sumit Agarwal <92676511+CrackingPanda@users.noreply.github.com>
2021-12-05 17:32:48 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,1 @@
-// import using require
-
-// declare class
-
-
-// export class using module.exports
+// import using require const Shape=require('./shape'); // declare class class Circle extends Shape { calculateArea() {}; } // export class using module.exports module.exports=Circle;
